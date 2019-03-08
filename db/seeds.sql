7079935072bcc0c399f5db0c3734dd8189f9840e
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-03-07 21:29:42 -0600

    might work

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Big burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Small burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Teeny burger");
